18c3474b49ba16eea51d63f3f83698dfadefe959
Yuvraj <yuvrajs22it@student.mes.ac.in>
2024-07-07 17:05:42 +0530

    Animation added.

---
+++ assets/js/gsapAnimation.js
@@ -0,0 +1,55 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline();
+
+// Animation for ".info" element
+tl.from(".info", {
+  duration: 1,
+  opacity: 0,
+  x: -500,
+});
+
+// Animation for ".img-anim" element
+tl.from(".img-anim", {
+  duration: 1,
+  opacity: 0,
+  x: 500,
+  overflow: "hidden",
+});
+
+// Animation for ".features-card" elements with ScrollTrigger
+gsap.from(".features-card", {
+  duration: 1,
+  opacity: 0,
+  scale: 0,
+  scrollTrigger: {
+    trigger: ".features-card",
+    start: "top 60%",
+    end: "top 30%",
+    toggleActions: "restart none none none",
+  },
+});
+
+gsap.from(".upper-text", {
+  duration: 1,
+  opacity: 0,
+  y: -400,
+  scrollTrigger: {
+    trigger: ".upper-text",
+    start: "top 60%",
+    end: "top 30%",
+    toggleActions: "restart none none none",
+  },
+});
+
+gsap.from([".lower-div", ".dotted-line"], {
+  duration: 1,
+  opacity: 0,
+  y: 400,
+  scrollTrigger: {
+    trigger: ".upper-text",
+    start: "top 60%",
+    end: "top 10%",
+    toggleActions: "restart none none none",
+  },
+});
